1d3a927c0eb55d712feeaff31dcdaf04d65e67df
sakai419 <sakaitaiki.soccer@icloud.com>
2024-11-05 21:00:51 +0900

    Modify login page

--- local-test/src/features/login/components/LoginContainer/index.tsx
+++ local-test/src/features/login/components/LoginContainer/index.tsx
@@ -1,10 +1,8 @@
-export default function LoginContainer({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+import { ReactNode } from "react";
+
+export default function LoginContainer({ children }: { children: ReactNode }) {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+		<div className="flex items-center justify-center min-h-screen bg-gray-100">
 			{children}
 		</div>
 	);
--- local-test/src/features/login/components/PasswordInput/index.tsx
+++ local-test/src/features/login/components/PasswordInput/index.tsx
@@ -20,11 +20,12 @@ export default function PasswordInput({
 					type={showPassword ? "text" : "password"}
 					placeholder="••••••••"
 					required
+					className="pr-10 w-full"
 				/>
 				<button
 					type="button"
 					onClick={togglePasswordVisibility}
-					className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 focus:outline-none"
+					className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
 					aria-label={
 						showPassword ? "パスワードを隠す" : "パスワードを表示"
 					}
