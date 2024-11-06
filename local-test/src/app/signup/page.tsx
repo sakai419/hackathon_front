fd0920af23c50a22c9e43a5c007554cb7e72e38a
sakai419 <sakaitaiki.soccer@icloud.com>
2024-11-06 19:14:18 +0900

    Add signup temp page

---
+++ local-test/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignupPage from "@/components/pages/SignupPage";
+
+export default function Signup() {
+	return (
+		<>
+			<SignupPage />
+		</>
+	);
+}
--- local-test/src/features/login/components/SignUpLink/index.tsx
+++ local-test/src/features/login/components/SignUpLink/index.tsx
@@ -2,7 +2,7 @@ export default function SignUpLink() {
 	return (
 		<div className="text-sm text-center text-gray-500">
 			アカウントをお持ちでない場合は
-			<a href="#" className="text-blue-600 hover:underline">
+			<a href="/signup" className="text-blue-600 hover:underline">
 				新規登録
 			</a>
 			してください
