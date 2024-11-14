3aa73d24a44434108b45f6c9b42b8d90ee422392
sakai419 <sakaitaiki.soccer@icloud.com>
2024-11-14 18:46:34 +0900

    Modify login page

--- local-test/src/components/pages/Login/index.tsx
+++ local-test/src/components/pages/Login/index.tsx
@@ -1,5 +1,6 @@
 import FormCard from "@/components/elements/FormCard";
 import LoginCardContent from "./components/LoginCardContent";
+import SignUpLink from "./components/SignUpLink";
 
 export default function LoginPage() {
 	return (
@@ -11,18 +12,7 @@ export default function LoginPage() {
 				title="ログイン"
 				description="アカウントにログインしてください"
 				content={<LoginCardContent />}
-				footer={
-					<div className="text-sm text-center text-gray-500">
-						アカウントをお持ちでない場合は、
-						<a
-							href="/signup"
-							className="text-blue-600 hover:underline"
-						>
-							新規登録
-						</a>
-						してください
-					</div>
-				}
+				footer={<SignUpLink />}
 			/>
 		</div>
 	);
