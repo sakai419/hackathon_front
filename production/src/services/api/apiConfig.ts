100d7010dbd50bd97048a9b51a344fc7c209b5fa
sakai419 <sakaitaiki.soccer@icloud.com>
2024-12-04 03:29:59 +0900

    Add public environmental var

--- production/src/services/api/apiConfig.ts
+++ production/src/services/api/apiConfig.ts
@@ -1,5 +1,4 @@
-export const API_BASE_URL =
-	"https://hackathon-back-352644019193.us-central1.run.app/api/v1";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const AUTH_ENDPOINT = `${API_BASE_URL}/auth`;
 export const ACCOUNT_ENDPOINT = `${API_BASE_URL}/accounts`;
 export const USER_ENDPOINT = `${API_BASE_URL}/users`;
