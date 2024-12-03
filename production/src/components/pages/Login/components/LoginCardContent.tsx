71de268f213cd618db1388d78b82c72f2e2e7e38
sakai419 <sakaitaiki.soccer@icloud.com>
2024-12-03 20:15:55 +0900

    Modify login card

--- production/src/components/pages/Login/components/LoginCardContent.tsx
+++ production/src/components/pages/Login/components/LoginCardContent.tsx
@@ -4,6 +4,7 @@ import { Input, Button } from "@/components/ui";
 import { useState } from "react";
 import { EyeOffIcon, EyeIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { ButtonWithTooltip } from "@/components/common";
 
 export default function LoginCardContent() {
 	const [showPassword, setShowPassword] = useState(false);
@@ -43,28 +44,28 @@ export default function LoginCardContent() {
 					<Input
 						id="password"
 						type={showPassword ? "text" : "password"}
-						placeholder="••••••••"
+						placeholder="password1234"
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 						required
 						className="pr-10 w-full"
 					/>
-					<button
-						type="button"
-						onClick={() => setShowPassword(!showPassword)}
-						className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
-						aria-label={
+					<ButtonWithTooltip
+						description={
 							showPassword
 								? "パスワードを隠す"
 								: "パスワードを表示"
 						}
-					>
-						{showPassword ? (
-							<EyeOffIcon className="h-5 w-5" />
-						) : (
-							<EyeIcon className="h-5 w-5" />
-						)}
-					</button>
+						onClick={() => setShowPassword(!showPassword)}
+						buttonClassName="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+						content={
+							showPassword ? (
+								<EyeOffIcon className="h-5 w-5" />
+							) : (
+								<EyeIcon className="h-5 w-5" />
+							)
+						}
+					/>
 				</div>
 			</div>
 			{isError && (
