5c495f135091bb1a97dde44b073b6ee27d1f9997
sakai419 <sakaitaiki.soccer@icloud.com>
2024-12-07 15:41:03 +0900

    Modify button style

--- local-test/src/components/pages/Login/components/LoginCardContent.tsx
+++ local-test/src/components/pages/Login/components/LoginCardContent.tsx
@@ -61,6 +61,7 @@ export default function LoginCardContent() {
 								"absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none",
 							onClick: () => setShowPassword(!showPassword),
 							variant: "ghost",
+							type: "button",
 						}}
 						content={
 							showPassword ? (
--- production/src/components/pages/SignUp/components/SignUpCardContent.tsx
+++ production/src/components/pages/SignUp/components/SignUpCardContent.tsx
@@ -83,6 +83,7 @@ export default function SignUpCardContent({
 							className:
 								"absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none",
 							variant: "ghost",
+							type: "button",
 						}}
 						content={
 							showPassword ? (
@@ -117,6 +118,7 @@ export default function SignUpCardContent({
 								"absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none",
 							onClick: () => setShowPassword(!showPassword),
 							variant: "ghost",
+							type: "button",
 						}}
 						content={
 							showPassword ? (
